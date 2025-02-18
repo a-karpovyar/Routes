dd4af4815dae9ed27a56f80448401eec6b4bc64a
Anatoly Karpov <tolick.karpow@mail.ru>
2025-02-18 20:20:56 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import { Routes, Route, Link } from "react-router-dom";
+import { Header } from "./components/Header";
+import { Footer } from "./components/Footer";
+
+import { About } from "./pages/About";
+import { Home } from "./pages/Home";
+import { Contacts } from "./pages/Contacts";
+import { NotFound } from "./pages/NotFound";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <header>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/contacts">Home</Link>
+      </header>
+      <main className=" content conatainer">
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/about" element={<About/>}/>
+          <Route path="/contacts" element={<Contacts/>}/>
+          <Route path="*" element={<NotFound/>}/>
+
+        </Routes>
+      </main>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
